141486c51640465ce228a9e819cb9f30d81eac18
Monika <j0h@example.com>
2022-06-15 00:49:45 +0100

    added the by locators and logic as suggested by Naveen

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,9 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
+	
+	private By cart = By.id("by.cart");
+	private By item = By.id("item-it");
 	
 	public void printcart() {
 		System.out.println("This is my cart page");
+		System.out.println("click on cart");
 	}
 
 }
