7a89b48df6290b5d79e0007d26968fb8f6da2c90
Monika <j0h@example.com>
2022-06-20 12:19:08 +0100

    adding cart pages changes

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -6,10 +6,15 @@ public class CartPage {
 	
 	private By cart = By.id("by.cart");
 	private By item = By.id("item-it");
+	private By item2 = By.id("item-it");
+	private By item3 = By.id("item-it");
+	
+	
 	
 	public void printcart() {
 		System.out.println("This is my cart page");
 		System.out.println("click on cart");
+		System.out.println("monika");
 	}
 
 }
